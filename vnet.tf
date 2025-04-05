252aba0a85358ed5a36eacbef16db5a22c26dff8
Stéphane Montri <stephane.montri@gmail.com>
2025-04-05 15:50:09 +0200

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,9 +1,9 @@
 # Virtual Network
-resource "azurerm_virtual_network" "ci-cd-vnet" {
+resource "azurerm_virtual_network" "${var.prefix}-vnet" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.ci-cd-rg.location
-  resource_group_name = azurerm_resource_group.ci-cd-rg.name
+  location            = azurerm_resource_group.${var.prefix}d-rg.location
+  resource_group_name = azurerm_resource_group.${var.prefix}-rg.name
 
   tags = {
     environment = "${var.environment}"
