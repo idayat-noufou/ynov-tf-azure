33725cd5d51d49020b954dc1ac9ac649c0683654
Stéphane MONTRI <stephane@StephaneMacBook.local>
2025-04-05 17:18:35 +0200

    add backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = azurerm_resource_group.ynov1-rg.name
+    storage_account_name = "your_storage_account_name"
+    container_name       = "your_container_name"
+    key                  = "ynov.tfstate"
+    tenant_id            = "38e72bba-3c22-4382-9323-ac1612931297"
+    subscription_id      = "c10b661f-7a09-44f4-b6c5-aa37c4d5263c"
+  }
+}
