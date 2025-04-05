cd946f35df0a40b5185c83201ea0626eb3ebc8c6
Stéphane MONTRI <stephane@StephaneMacBook.local>
2025-04-05 20:15:21 +0200

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = azurerm_resource_group.ynov1-rg.name
+    resource_group_name  = "smontri-resources"
     storage_account_name = "your_storage_account_name"
     container_name       = "your_container_name"
     key                  = "ynov.tfstate"
